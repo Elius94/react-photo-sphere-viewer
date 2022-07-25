41d6aa6241f75b8c7db94aef5fb3de7021f08508
Elia Lazzari <elia.lazzari@gmail.com>
2022-07-25 12:00:32 +0200

    fixed issues

--- rollup.config.js
+++ rollup.config.js
@@ -6,36 +6,38 @@ import url from "@rollup/plugin-url"
 import svgr from "@svgr/rollup"
 import babel, { getBabelOutputPlugin } from "@rollup/plugin-babel"
 
-import pkg from "./package.json"
-
-export default {
-    input: "src/index.tsx",
-    output: [{
-        file: pkg.main,
-        format: "cjs",
-        exports: "auto",
-        sourcemap: true
-    },
-    {
-        file: pkg.module,
-        format: "esm",
-        exports: "auto",
-        sourcemap: true
-    },
+export default [{
+    input: [
+        "src/index.tsx",
+    ],
+    output: [
+        {
+            dir: "dist",
+            format: "esm",
+            exports: "named",
+            preserveModules: true,
+            preserveModulesRoot: "src",
+            sourcemap: true,
+        }
     ],
     plugins: [
+        resolve(),
         postcss({
             modules: false
         }),
+        commonjs(),
         url(),
         svgr(),
-        resolve(),
-        typescript(),
-        commonjs(),
+        typescript({
+            tsconfig: "./tsconfig.json",
+            declaration: true,
+            declarationDir: "dist",
+        }),
         babel({ exclude: "node_modules/**", presets: ["@babel/preset-react"], babelHelpers: "bundled" }),
         getBabelOutputPlugin({
-            presets: ["@babel/preset-env"]
-        })
+            presets: ["@babel/preset-react","@babel/preset-env"],
+            plugins: [["babel-plugin-styled-components", { displayName: true }]],
+        }),
     ],
     external: ["react", "react-dom"],
-}
+}]
